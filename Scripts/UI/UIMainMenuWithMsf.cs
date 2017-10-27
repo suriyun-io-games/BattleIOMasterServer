a3404c4658f1e5860b71148c947fb68866bc2e25
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-27 23:11:17 +0700

    Master server WIP

---
+++ Scripts/UI/UIMainMenuWithMsf.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Barebones.MasterServer;
+
+public class UIMainMenuWithMsf : UIMainMenu
+{
+    public UINetworkRoom networkRoomDialog;
+    public void OnClickRoomList()
+    {
+        if (networkRoomDialog != null)
+            networkRoomDialog.Show();
+    }
+
+    public void OnClickDedicate()
+    {
+        var master = FindObjectOfType<MasterServerBehaviour>();
+        master.StartServer();
+        var spawner = FindObjectOfType<SpawnerBehaviour>();
+        spawner.StartSpawner();
+    }
+}

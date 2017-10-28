c88e99d158836eecb4b5494d4d4c239a3c3b486b
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2017-10-28 14:39:17 +0700

    Master server WIP

---
+++ Scripts/Networking/GameNetworkManagerWithMsf.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameNetworkManagerWithMsf : GameNetworkManager
+{
+    public override void OnServerDisconnect(NetworkConnection conn)
+    {
+        base.OnClientDisconnect(conn);
+        var ioGamesRoom = GetComponent<IOGamesRoom>();
+        if (ioGamesRoom != null)
+            ioGamesRoom.ClientDisconnected(conn);
+    }
+}
